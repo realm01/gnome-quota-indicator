9f2ece0628ef7d0560d8372e3685db54831c1866
Anastassios Martakos <amartako@phys.ethz.ch>
2016-12-20 15:20:25 +0100

    added WindowViewBase

--- lib/mvc/bases.py
+++ lib/mvc/bases.py
@@ -1,7 +1,8 @@
 """MVC Base classes."""
 
-from lib.helpers import load_config
+from lib.helpers import load_config, getuid, get_path
 from lib.exception_feedback import show_cmd_error
+import os
 
 
 class ModelBase():
@@ -20,6 +21,31 @@ class ViewBase():
         self.app = app
         self.model = model
 
+
+class WindowViewBase(ViewBase):
+    """Base class of all windows."""
+
+    def __init__(self, app, model):
+        """Ctor of WindowViewBase."""
+        super().__init__(app, model)
+
+        self.set_title(self.app.name)
+        self.set_resizable(False)
+        self.resize(200, 400)
+        self.set_icon_from_file(self.getIcon())
+
+    def getIcon(self):
+        """Retrieve the path to the icon from /tmp with the UID prepended."""
+        failed = False
+        try:
+            p = '/tmp/' + getuid() + '_compiled.png'
+        except Exception as e:
+            failed = True
+
+        if not os.path.exists(p) or failed:
+            return get_path('../img/icon_default.png')
+        else:
+            return p
 
 class ControllerBase():
     """Base class of all Controllers."""
